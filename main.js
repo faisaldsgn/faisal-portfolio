a032295c10e25f12afe938c4451bd741d684e502
Faisal Almahmoud <faisal.almahmoud06@gmail.com>
2022-01-30 21:43:24 +0300

    add portfolio

---
+++ main.js
@@ -0,0 +1,17 @@
+// import './style.css'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+
+
+// window.Alpine = Alpine
+// Alpine.start()
+
+// window.intersect = Alpine
+// Alpine.plugin(intersect)
+
+$(window).load(function() {
+    $("body").removeClass("preload");
+  });
