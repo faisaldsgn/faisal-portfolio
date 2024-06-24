4a320ca2cef34f9f37efce4b063207aae6217565
Faisal Almahmoud <faisal.almahmoud06@gmail.com>
2024-06-24 19:02:26 +0300

    back to cli

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'faisal-brand': '#7D7AFB',
+      }
+    },
+  },
+  plugins: [],
+}
